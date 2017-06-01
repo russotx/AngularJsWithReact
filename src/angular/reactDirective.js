ebde526594ca0fd4e67a7bbc79c89028d17c0950
Daniel Russo <Daniel@Daniels-MBP.example.com>
2017-06-01 15:30:55 -0500

    cleaned up style and added comments explaining communication between AngularJS and React

--- src/angular/reactDirective.js
+++ src/angular/reactDirective.js
@@ -8,23 +8,41 @@ const reactDirective = app.directive('reactDirective', function() {
       template: '<div id="reactapp" class="react-part"></div>',
       scope: {
         todos: '=',
+        // markComplete is a function that we'll pass to React as props
         markComplete:'&'
       },
       link: function(scope, el, attrs){
-            scope.newItem = (value) => {alert (value)}
-            // scope.markComplete = (todoItem) => {scope.markItemCompleted(todoItem)}
+              // newItem is a function that fires an alert, the value is a string passed from React in Layout.js        
+              scope.newItem = (value) => alert (value);
 
-            const reactapp = document.getElementById('reactapp')
-            scope.$watch('todos', function(newValue, oldValue) {
-                if (angular.isDefined(newValue)) {
-                 ReactDOM.render(
-                    <Layout todos={newValue} newItem={scope.newItem} markComplete={scope.markComplete}/>
-                    , reactapp);
-                }
-            }, true);
+              // scope.markComplete = (todoItem) => {scope.markItemCompleted(todoItem)}
+              const reactapp = document.getElementById('reactapp');
 
-        }
+              scope.$watch('todos', 
+                // callback function to run every time 'todos' changes
+                // checks if newValue exists
+                function(newValue, oldValue) {
+                  if (angular.isDefined(newValue)) {
+                    // render the react app every time the value changes
+                    // only renders in the virtual DOM, React won't render anything in the real DOM unless 
+                    // the React diffing algorithm sees a change.
+                    ReactDOM.render(
+                      // passing newValue and scope properties from Angular directive to React component as props.
+                      // scope.markComplete is a function passed in as props
+                      // Layout component is defined in Layout.js, maps todos to li components which fire markComplete when clicked
+                      // and adds buttons that fire newItem
+                      <Layout 
+                        todos={newValue} 
+                        newItem={scope.newItem} 
+                        markComplete={scope.markComplete}
+                      />, 
+                      reactapp);
+                  }
+                }, 
+                true);
+
+            }
     }
-})
+});
 
 export default reactDirective
--- src/react/Layout.js
+++ src/react/Layout.js
@@ -11,19 +11,25 @@ export default class Layout extends React.Component{
 	render(){	
 		return (
 			<div>
-				<hr/>
-				<h1>React TO-DO</h1>
-				<ul>{this.props.todos.map((todo,key)=>{
-					return <li key={key} onClick={ (event) => {
-									  this.props.markComplete(todo)
-									}
-								} className={["list-item", todo.done === true? "done-true":"done-false"].join(" ")}>{todo.text}
-						    </li>})
-				}</ul>
-				<button id="click" onClick={ () => {
-						this.props.newItem("Alter triggered from React but Fired from AngularJS")
-					  }
-					}>Click to make Angular Alert!!</button>
+              <hr/>
+              <h1>React TO-DO</h1>
+              <ul>
+                {this.props.todos.map((todo,key) =>
+                  <li 
+                    key={key} 
+                    onClick={ (event) => this.props.markComplete(todo) } 
+                    className={["list-item", todo.done === true ? "done-true" : "done-false"].join(" ")}
+                  >
+                    {todo.text}
+                  </li>)
+                }
+              </ul>
+              <button 
+                id="click" 
+                onClick={ () => this.props.newItem("Alter triggered from React but Fired from AngularJS")}
+              >
+                Click to make Angular Alert!!
+              </button>
 			</div>
 		);
 	}
